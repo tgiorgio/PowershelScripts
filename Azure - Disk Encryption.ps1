540c32e546eaebb9176c0af659e1e86297bbdc1b
Thiago Di Giorgio <thiago.digiorgio@version1.com>
2019-02-22 10:20:12 +0000

    k

--- Azure - Disk Encryption.ps1
+++ Azure - Disk Encryption.ps1
@@ -1,8 +1,18 @@
-$rgName = 'LIVE-JUMPBOX';
- $vmName = 'EUNLIVEWJMP01';
- $KeyVaultName = 'Oir-LIVE-KeyVault';
- $KeyVault = Get-AzKeyVault -VaultName $KeyVaultName -ResourceGroupName "LIVE-KeyVault";
- $diskEncryptionKeyVaultUrl = $KeyVault.VaultUri;
- $KeyVaultResourceId = $KeyVault.ResourceId;
+$rgName = 'siaedevops_rg';
+$vmName = 'siaedevops01';
+$KeyVaultName = 'siadevencrypt';
+$KeyVault = Get-AzKeyVault -VaultName $KeyVaultName -ResourceGroupName "siadevencrypt-rg";
+$diskEncryptionKeyVaultUrl = $KeyVault.VaultUri;
+$KeyVaultResourceId = $KeyVault.ResourceId;
 
- Set-azurermVMDiskEncryptionExtension -ResourceGroupName $rgname -VMName $vmName -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl -DiskEncryptionKeyVaultId $KeyVaultResourceId;    
+$AADClientName = "EncryptionApp"
+$AADClientSecret = "5T7OKpb/3mY2JojlYkQqYh4mgzUkhB2b9gFlG5Td3oc="
+
+$SvcPrincipals = (Get-AzADServicePrincipal -SearchString $AADClientName)
+$AADClientID = $SvcPrincipals.ApplicationId
+
+#To encryption WITHOUT azure AD Web Application
+ Set-AzVMDiskEncryptionExtension -ResourceGroupName $rgname -VMName $vmName -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl -DiskEncryptionKeyVaultId $KeyVaultResourceId;    
+
+ #To encryption WITH azure AD Web Application
+ Set-AzVMDiskEncryptionExtension -ResourceGroupName $rgName -VMName $vmName -AadClientID $AADClientID -AadClientSecret $AADClientSecret -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl -DiskEncryptionKeyVaultId $KeyVaultResourceId -VolumeType 'All' -SkipVmBackup -Confirm:$false -Force -ErrorAction Continue
